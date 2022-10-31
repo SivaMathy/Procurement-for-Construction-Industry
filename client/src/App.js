e564c0d320ad5236ad9c1f69acde0ae53d1011f6
priya_19 <it20141042@my.sliit.lk>
2022-10-31 22:10:25 +0530

    changes

--- client/src/App.js
+++ client/src/App.js
@@ -22,7 +22,7 @@ import CrudEdit from "./componets/cruds/CrudEdit";
 import CrudDelete from "./componets/cruds/CrudDelete";
 
 import AddnewDoctor from "./componets/cruds/AddnewDoctor";
-import Doctorcrudtable from "./componets/cruds/Doctorcrudtable"
+import Doctorcrudtable from "./componets/cruds/Doctorcrudtable";
 import ThHome from "./componets/pages/ThHome";
 // Mathy
 import AddMedicines from "./componets/m_AddMedicines";
@@ -34,7 +34,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "antd/dist/antd.css";
 import P_Dashboard from "./P_Views/P_Dashboard";
 import P_ViewAllAppointments from "./P_Views/P_ViewAllAppointments";
-import P_Booking from "./P_Views/P_Booking";
+
 import P_Form from "./P_Views/P_Form";
 import P_UserEdit from "./P_Views/P_UserEdit";
 import P_Details from "./P_Views/P_Details";
@@ -57,32 +57,31 @@ function App() {
           <Route path="/editProgram/:id" element={<EditProgram />} />
           <Route path="/createProgram" element={<CreateProgram />} />
           <Route path="/patientsProgram" element={<PatientsProgeam />} />
-            <Route exact path="/thhome" element={<ThHome/>} />	
-          <Route exact path="/cruds" element={<CrudTable />} />					
-					<Route exact path="/cruds/list-view" element={<CrudListView />} />
-					<Route exact path="/cruds/grid-view" element={<CrudGridView />} />
-					<Route exact path="/cruds/new" element={<CrudAdd />} />
-					<Route exact path="/cruds/Doc" element={<AddnewDoctor />} />
-					<Route exact path="/cruds/:_id" element={<CrudDetails />} />
-					<Route exact path="/cruds/:_id/edit" element={<CrudEdit />} />
-					<Route exact path="/cruds/:_id/delete" element={<CrudDelete />} />
-					<Route exact path="/cruds/tab" element={<Doctorcrudtable />} />
-					
+          <Route exact path="/thhome" element={<ThHome />} />
+          <Route exact path="/cruds" element={<CrudTable />} />
+          <Route exact path="/cruds/list-view" element={<CrudListView />} />
+          <Route exact path="/cruds/grid-view" element={<CrudGridView />} />
+          <Route exact path="/cruds/new" element={<CrudAdd />} />
+          <Route exact path="/cruds/Doc" element={<AddnewDoctor />} />
+          <Route exact path="/cruds/:_id" element={<CrudDetails />} />
+          <Route exact path="/cruds/:_id/edit" element={<CrudEdit />} />
+          <Route exact path="/cruds/:_id/delete" element={<CrudDelete />} />
+          <Route exact path="/cruds/tab" element={<Doctorcrudtable />} />
           {/* Mathy */}
-          <Route exact path='/addmed' element={<AddMedicines/>}/>
-          <Route path='/show' element={<MedicinesAdmin/>}/>
-          <Route path='/delivery' element={<DeliveryDetails/>}/>
-          <Route  path="/pay" element={<Pay/>} />
-          <Route path='/phar' element={<Pharmacy/>}/>
+          <Route exact path="/addmed" element={<AddMedicines />} />
+          <Route path="/show" element={<MedicinesAdmin />} />
+          <Route path="/delivery" element={<DeliveryDetails />} />
+          <Route path="/pay" element={<Pay />} />
+          <Route path="/phar" element={<Pharmacy />} />
           <Route path="/labdash" element={<P_Dashboard />} />
-          <Route path="/book" element={<P_Booking />} />
           <Route path="/" element={<P_ViewAllAppointments />} />
           <Route path="/form" element={<P_Form />} />
           <Route path="/edit/:id" element={<P_UserEdit />} />
           <Route path="/view" element={<P_Details />} />
           <Route path="/editdate/:id" element={<P_AssignDate />} />
           <Route exact path="/cruds/:_id/dedit" element={<Doctoredit />} />
-          <Route exact path="/cruds/:_id/ddelete" element={<Doctordelete />} />
+                   {" "}
+          <Route exact path="/cruds/:_id/ddelete" element={<Doctordelete />} />
         </Routes>
       </BrowserRouter>
     </div>
--- client/src/P_Views/P_Booking.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function P_Booking() {
-  return <div></div>;
-}
-
-export default P_Booking;
